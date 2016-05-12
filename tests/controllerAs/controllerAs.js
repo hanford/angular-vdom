eb82ef288edca776d78eb05db204009b4692ab0c
Jack Hanford <jackhanford@gmail.com>
2016-05-12 15:53:03 -0700

    add basic support for guessing controllerAs variables (#7)
    refs #3

---
+++ tests/controllerAs/controllerAs.js
@@ -0,0 +1,12 @@
+var virtualComponent = require('../../index.js')
+var render = require('./virtual-dom.component')
+
+var countComponent = virtualComponent(render, {bindings: {count: '<'}})
+
+module.exports = require('angular')
+  .module('example1', [])
+  .component('counter', countComponent)
+  .controller('ctrl', function () {
+    this.count = 0
+  })
+  .name
---
+++ tests/controllerAs/index.js
@@ -0,0 +1,30 @@
+var expect = require('chai').expect
+var virtualComponent = require('../../index.js')
+
+var exampleApp = require('./controllerAs')
+
+var angular = require('angular')
+
+describe('angular-vdom', function () {
+  var $compile
+  var scope
+  var element
+  var $rootScope
+
+  beforeEach(angular.mock.module(exampleApp))
+
+  beforeEach(inject(function (_$compile_, _$rootScope_) {
+    element = angular.element("<div ng-controller='ctrl as app'><counter count='app.count'></counter></div>")
+    $compile = _$compile_
+    $rootScope = _$rootScope_
+    scope = $rootScope.$new()
+
+    $compile(element)(scope)
+
+    scope.$digest()
+  }))
+
+  it('Replaces the element with the appropriate content, with controllerAs', function () {
+    expect(element.html()).to.equal('<counter count="app.count" class="ng-isolate-scope"><div>virtual-dom: 0</div></counter>')
+  })
+})
